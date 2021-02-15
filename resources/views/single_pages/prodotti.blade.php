43078cb18a752397a0a22f0eb9c5eaaef5f77a92
Romina Trazzi <romina.trazzi@posta.it>
2021-02-15 09:18:34 +0100

    Commit refactoring (aggiunto partial pasta)

--- resources/views/single_pages/prodotti.blade.php
+++ resources/views/single_pages/prodotti.blade.php
@@ -7,55 +7,9 @@
 @section('main')
 <main>
     <div class="container">
-        <h2> LE LUNGHE </h2>
-        <div class ="container_pasta">
-            @foreach($data as $dato)
-                @if ($dato['tipo'] == "lunga")
-                    <div class="box_pasta">
-                        <img src= "{{ $dato['src'] }}" alt = "formato di pasta lunga">
-                        <div class="content">
-                            <p> "{{ $dato['titolo'] }}" </p>
-                            <img src="{{ asset('img/icon.svg') }}">
-                        </div>  
-                    </div>
-                @endif
-            @endforeach 
-        </div>
+      
+      @include('partials.pasta');
 
-        <h2> LE CORTE </h2>
-        <div class ="container_pasta">
-            @foreach($data as $dato)
-                @if ($dato['tipo'] == "corta")
-                    <div class="box_pasta">
-                        <img src= "{{ $dato['src'] }}" alt = "formato di pasta corta">
-
-                        <div class="content">
-                            <p>"{{ $dato['titolo'] }}"</p>
-                            <img src="{{ asset('img/icon.svg') }}">
-                        </div>
-
-                    </div>
-                @endif
-            @endforeach 
-        </div>
-
-        <h2> LE CORTISSIME </h2>
-        <div class ="container_pasta">
-            
-            @foreach($data as $dato)
-                @if ($dato['tipo'] == "cortissima")
-                    <div class="box_pasta">
-                        <img src= "{{ $dato['src'] }}" alt = "formato di pasta cortissima">
-
-                        <div class="content">
-                            <p> "{{ $dato['titolo'] }}" </p>
-                            <img src="{{ asset('img/icon.svg') }}">
-                        </div>
-
-                    </div>
-                @endif
-            @endforeach 
-        </div>
     </div>
 </main>
 @endsection
